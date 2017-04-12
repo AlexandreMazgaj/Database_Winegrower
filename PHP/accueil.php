70924fe44017f6176ab3458d11f9e3cd7e4059e1
AlexandreMazgaj <mazgaj.alexandre@gmail.com>
2017-04-12 23:55:50 +0200

    Create accueil.php

---
+++ PHP/accueil.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>Bienvenue sur le site du projet de NF17 "Exploitation Viticole"</h1>
+		<h3>Il vous permet d'interagir avec la base de données (ajout d'éléments, requêtes, ...).</h3>
+		<h3>Veuillez pour cela choisir un des éléments du menu de gauche.</h3>
+	</body>
+</html>
